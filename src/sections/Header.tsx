56b0960cd80f36102df7c5e31962ba3a7b39f2e4
PriyanshuYadav08 <priyanshu81104@gmail.com>
2025-04-14 00:29:50 +0530

    sun_start_gng

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,21 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+  return (
+    <div className="flex justify-center items-center relative top-3">
+      <nav>
+        <a href="/home"
+           className="px-4 py-1.5 rounded-full text-white/70 text-sm font-semibold ">
+          Home
+        </a>
+        <a href="/about">
+          About
+        </a>
+        <a href="/projects">
+          Projects
+        </a>
+        <a href="/contact">
+          Contact
+        </a>
+      </nav>
+    </div>
+  );
 };
